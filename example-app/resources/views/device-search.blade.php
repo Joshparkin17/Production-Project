414864df038c4cc4b2ce619023ff7c1a97b13a02
Joshparkin17 <73292744+Joshparkin17@users.noreply.github.com>
2023-05-16 21:04:14 +0100

    Delete example-app directory

--- example-app/resources/views/device-search.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    
-<div class="grid grid-cols-1 md:grid-cols-2  gap-4">
-            @foreach($devices as $device)
-                <x-device-card :device="$device" /> 
-            @endforeach  
-        </div>      
-
-</x-app-layout>
